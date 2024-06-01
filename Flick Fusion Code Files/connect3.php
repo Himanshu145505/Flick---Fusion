b2abc3f3451f9eac1ada341e9f96af049f60bdbc
Himanshu Sharma <himanshusharma24795@gmail.com>
2024-06-01 10:35:13 +0530

    Update connect3.php

--- Flick Fusion Code Files/connect3.php
+++ Flick Fusion Code Files/connect3.php
@@ -1,14 +1,19 @@
+<!--Connectivity Details -->
+
 <?php
 $Name = $_POST['Name'];
 $Email = $_POST['Email'];
 $Password = $_POST['Password'];
 $ConfirmPassword = $_POST['ConfirmPassword'];
 
+// Connectivity Check
+
 $conn = new mysqli('127.0.0.1:3308', 'root', '', 'OTTPLAT');
 if ($conn->connect_error) {
     echo "$conn->connect_error";
     die("Connection Failed : " . $conn->connect_error);
 } else {
+    // Insertion of registration data into database
     $stmt = $conn->prepare("insert into Data(Name, Email, Password, ConfirmPassword) values(?, ?, ?, ?)");
     $stmt->bind_param("ssss", $Name, $Email, $Password, $ConfirmPassword);
     $execval = $stmt->execute();
